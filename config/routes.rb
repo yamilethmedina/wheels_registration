02799925a9a7902e7b6da94365dd9040141601a5
Yami Medina <yamilethmedina1@gmail.com>
2015-04-03 18:46:06 +0000

    generated profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
  devise_for :users, :controllers => { registrations: 'registrations' }
    devise_scope :user do
      get "register", to: "devise/registrations#new", as: :register
---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+ test "that /login route opens the login page" do
+      get "/login"
+      assert_response :success
+ end
+  #   assert true
+  # end
+end
