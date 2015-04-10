096a95e274167cbffdc8097d14b73fd3d8f80281
Yami Medina <yamilethmedina1@gmail.com>
2015-04-10 18:51:50 +0000

    Still not quite right with individual profile pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,27 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.where(profile_name: params[:user_id]).limit(1).first
-    if @user
-      @statuses = @user.statuses.all
+    if user
       render action: :show
     else
       render file: "public/404", status: 404, formats: [:html]
     end
   end
+
+  private
+
+  def user
+    @user = User.where(profile_name: params[:profile_name]).limit(1).first
+  end
+
+  def statuses
+    user.statuses
+  end
+
+  helper_method :user, :statuses
 end
+
+def full_name
+    first_name + " " + last_name
+end
+
+
