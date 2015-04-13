5f9cb1d56afc02b74c6bf5c21b9fd9c9c1ae6393
Yami Medina <yamilethmedina1@gmail.com>
2015-04-13 18:30:47 +0000

    Attempting to add event checkbox fields to forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:profile_name, :first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:profile_name, :first_name, :last_name, :email, :password, :password_confirmation, :nov11underlinecommute, :fiuride, :brickellbikein, :nov12underlinecommute, :ecgwelcomeride, :emergetwilightride, :thursfootworksrun, :thursbrickellrun, :kirkmunroereception, :nov13underlinecommute, :ecgaloop, :friconference, :homesteadbikein, :ecgareception, :miamibikescene, :evergladesbikeclub1, :satfootworksrun, :ludlamtrailrun, :satconference, :emerge2ndsat, :gravelgrindersludlam, :southmiamibikein, :ebckirkmunroeride, :trilocokirkmunroeride, :stormriderskirkmunroeride, :bikegrovekirkmunroeride, :kirkmunroehonors, :bwcgcgmride, :ciclovia, :kidicalmass, :upghike, :ecgaboardmeeting, :scouttroopride, :ecgamembershipsummit)
   end
 
   def account_update_params
-    params.require(:user).permit(:profile_name, :first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:profile_name, :first_name, :last_name, :email, :password, :password_confirmation, :current_password, :nov11underlinecommute, :fiuride, :brickellbikein, :nov12underlinecommute, :ecgwelcomeride, :emergetwilightride, :thursfootworksrun, :thursbrickellrun, :kirkmunroereception, :nov13underlinecommute, :ecgaloop, :friconference, :homesteadbikein, :ecgareception, :miamibikescene, :evergladesbikeclub1, :satfootworksrun, :ludlamtrailrun, :satconference, :emerge2ndsat, :gravelgrindersludlam, :southmiamibikein, :ebckirkmunroeride, :trilocokirkmunroeride, :stormriderskirkmunroeride, :bikegrovekirkmunroeride, :kirkmunroehonors, :bwcgcgmride, :ciclovia, :kidicalmass, :upghike, :ecgaboardmeeting, :scouttroopride, :ecgamembershipsummit)
   end
 end
--- db/migrate/20150330160545_add_user_id_to_statuses.rb
+++ db/migrate/20150330160545_add_user_id_to_statuses.rb
@@ -1,6 +1,40 @@
 class AddUserIdToStatuses < ActiveRecord::Migration
   def change
     add_column :statuses, :user_id, :integer
+    add_column :users, :nov11underlinecommute, :boolean, default: false
+    add_column :users, :fiuride, :boolean, default: false
+    add_column :users, :brickellbikein, :boolean, default: false
+    add_column :users, :nov12underlinecommute, :boolean, default: false
+    add_column :users, :ecgwelcomeride, :boolean, default: false
+    add_column :users, :emergetwilightride, :boolean, default: false
+    add_column :users, :thursfootworksrun, :boolean, default: false
+    add_column :users, :thursbrickellrun, :boolean, default: false
+    add_column :users, :kirkmunroereception, :boolean, default: false
+    add_column :users, :nov13underlinecommute, :boolean, default: false
+    add_column :users, :ecgaloop, :boolean, default: false
+    add_column :users, :friconference, :boolean, default: false
+    add_column :users, :homesteadbikein, :boolean, default: false
+    add_column :users, :ecgareception, :boolean, default: false
+    add_column :users, :miamibikescene, :boolean, default: false
+    add_column :users, :evergladesbikeclub1, :boolean, default: false
+    add_column :users, :ludlamtrailrun, :boolean, default: false
+    add_column :users, :satfootworksrun, :boolean, default: false
+    add_column :users, :satconference, :boolean, default: false
+    add_column :users, :emerge2ndsat, :boolean, default: false
+    add_column :users, :scouttroopride, :boolean, default: false
+    add_column :users, :ecgamembershipsummit, :boolean, default: false
+    add_column :users, :gravelgrindersludlam, :boolean, default: false
+    add_column :users, :southmiamibikein, :boolean, default: false
+    add_column :users, :ebckirkmunroeride, :boolean, default: false
+    add_column :users, :trilocokirkmunroeride, :boolean, default: false
+    add_column :users, :stormriderskirkmunroeride, :boolean, default: false
+    add_column :users, :bikegrovekirkmunroeride, :boolean, default: false
+    add_column :users, :kirkmunroehonors, :boolean, default: false
+    add_column :users, :bwcgcgmride, :boolean, default: false
+    add_column :users, :ciclovia, :boolean, default: false
+    add_column :users, :kidicalmass, :boolean, default: false
+    add_column :users, :upghike, :boolean, default: false
+    add_column :users, :ecgaboardmeeting, :boolean, default: false
     add_index :statuses, :user_id
     remove_column :statuses, :name
   end
