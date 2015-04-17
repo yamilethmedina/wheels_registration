41e904ee95518228710b83425145c0daa5821adc
Yami Medina <yamilethmedina1@gmail.com>
2015-04-17 20:10:53 +0000

    db rake migrate error

--- db/migrate/20150415215651_create_events.rb
+++ db/migrate/20150415215651_create_events.rb
@@ -2,6 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :event_name
+      t.text :location
       t.date :event_date
       t.time :event_time
       t.text :description
---
+++ db/migrate/20150417195710_add_event_time_to_event.rb
@@ -0,0 +1,5 @@
+class AddEventTimeToEvent < ActiveRecord::Migration
+  def change
+    add_column :events, :event_time, :time
+  end
+end
