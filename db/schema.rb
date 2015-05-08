f8f68eddb5be22156d6b8119b4cea30741402c00
Yami Medina <yamilethmedina1@gmail.com>
2015-05-08 22:05:45 +0000

    changed from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150427200652) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: true do |t|
     t.string   "event_name"
     t.text     "location"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 20150427200652) do
     t.datetime "updated_at",         null: false
   end
 
-  add_index "participations", ["event_id"], name: "index_participations_on_event_id"
-  add_index "participations", ["user_id"], name: "index_participations_on_user_id"
+  add_index "participations", ["event_id"], name: "index_participations_on_event_id", using: :btree
+  add_index "participations", ["user_id"], name: "index_participations_on_user_id", using: :btree
 
   create_table "statuses", force: true do |t|
     t.text     "content"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150427200652) do
     t.integer  "user_id"
   end
 
-  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -94,7 +97,7 @@ ActiveRecord::Schema.define(version: 20150427200652) do
     t.boolean  "admin",                     default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
